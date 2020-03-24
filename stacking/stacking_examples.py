c48fcdfd83acd4f24ee8e4b97cf7153e2ffa3ecd
shuaiweixiaozi <shuaiweixiaozi@163.com>
2020-03-24 19:02:55 +0800

    stacking example 完成

--- stacking/stacking_examples.py
+++ stacking/stacking_examples.py
@@ -4,7 +4,16 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.naive_bayes import GaussianNB
 from sklearn.linear_model import LogisticRegression
 from mlxtend.classifier import StackingCVClassifier
+from mlxtend.plotting import plot_decision_regions
 from sklearn import model_selection
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+import itertools
+
+"""
+参考网址： https://rasbt.github.io/mlxtend/user_guide/classifier/StackingCVClassifier/#example-1-simple-stacking-cv-classification
+介绍机器学习中stacking技术，并给出相应例子
+"""
 
 iris = datasets.load_iris()
 X, y = iris.data[:, 1:3], iris.target
@@ -23,6 +32,14 @@ def simple_stacking_cv_classification(X, y):
     for clf, label in zip([clf1, clf2, clf3, sclf], ['KNN', 'Random Forest', 'Naive Bayes', 'StackingClassifier']):
         scores = model_selection.cross_val_score(clf, X, y, cv=3, scoring='accuracy')
         print("Accuracy: %.2f (+/- %0.2f) [%s]" % (scores.mean(), scores.std(), label))
+    gs = gridspec.GridSpec(2, 2)
+    fig = plt.figure(figsize=(10, 8))
+    for clf, lab, grd in zip([clf1, clf2, clf3, sclf], ['KNN', 'Random Forest', 'Naive Bayes', 'StackingClassifier'], itertools.product([0, 1], repeat=2)):
+        clf.fit(X, y)
+        ax = plt.subplot(gs[grd[0], grd[1]])
+        fig = plot_decision_regions(X=X, y=y, clf=clf)
+        plt.title(lab)
+    plt.show()
 
 
 if __name__ == '__main__':
